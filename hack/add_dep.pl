74bccf476fa828d7063b6585d7dc6908f19e59b2
Vincent van Dam <vincentd@erg.verweg.com>
2018-06-13 12:24:27 +0200

    Add autodetection of kie-api version, close #2

--- hack/add_dep.pl
+++ hack/add_dep.pl
@@ -27,20 +27,33 @@ sub getdep {
   return $dep;
 }
 
+## getpluginv will return the version of the configured maven kie plugin, if
+## present.
+sub getpluginv {
+  my $pom = shift;
+  my $ver = undef;
+  $pom =~ s#.*<plugins>##is;
+  $pom =~ s#</plugins>.*##is;
+  $pom =~ s#<plugin>#\t#gis;
+  foreach my $p (split(/\t/,$pom)) {
+    if ($p =~ m/kie-maven-plugin/) {
+      $ver = $p;
+      $ver =~ s#.*<version>##is;
+      $ver =~ s#<version>.*##is;
+      last;
+    }
+  }
+  return $ver;
+}
+
 ## main will move to the batmobile and go.
 sub main {
   my $pomfile = shift || $ENV{DEP_POMFILE};
-  my $version = shift || $ENV{DEP_KIE_API_VERSION};
   my $groupid = shift || $ENV{DEP_KIE_GROUP_ID} || "org.kie";
   my $artifact= shift || $ENV{DEP_KIE_ARTIFACT} || "kie-api";
 
-
   if (!$pomfile) {
     print STDERR "Warning: no pom file specified, not adding kie api dependency\n";
-    exit(0);
-  }
-  if (!$version) {
-    print STDERR "Warning: no kie api version specified, not adding kie api dependency\n";
     exit(0);
   }
 
@@ -59,6 +72,8 @@ sub main {
     print STDERR "Info: kie-maven-plugin dependency already available, nothing to do...\n";
     exit(0);
   }
+
+  my $version = getpluginv($pom) || "6.5.0.Final-redhat-2";
 
   my $dep = getdep($groupid,$artifact,$version);
   $pom =~ s#</dependencies>#$dep</dependencies>#is;
